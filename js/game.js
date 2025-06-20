cb6e95f6b4433e7bbaa08e25a2f94fa6a0e5d1e1
Arthur-codeX <daniel.karanja239@gmail.com>
2025-06-20 17:11:06 +0300

    updated game.js

---
+++ js/game.js
@@ -0,0 +1,8 @@
+const countdown = document.getElementById("countdown");
+
+const confuseThem = document.getElementById("confuse-them");
+const PlayBtn = document.getElementById("play-btn");
+
+// console.log(countdown);
+// console.log(confuseThem);
+// console.log(PlayBtn);

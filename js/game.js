b0d87cea6a5734922698978d495513eab0329e08
Arthur-codeX <daniel.karanja239@gmail.com>
2025-06-20 17:38:15 +0300

    updated random

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ const countdown = document.getElementById("countdown");
 const confuseThem = document.getElementById("confuse-them");
 const PlayBtn = document.getElementById("play-btn");
 
+let consufusionInterVal = null;
+
 let countDownValue = 15;
 
 // console.log(countdown);
@@ -22,4 +24,18 @@ function StartCountDown() {
     countdown.innerText = countDownValue - 1;
     countDownValue = countDownValue - 1;
   }, 1000);
+}
+
+function ConfusionStart() {
+  let arr = [
+    `<i class="fa-solid fa-hand-back-fist" style="font-size: 100px"></i>`,
+    `<i class="fa-solid fa-hand" style="font-size: 100px"></i>`,
+    `<i class="fa-solid fa-hand-scissors" style="font-size: 100px"></i>`,
+  ];
+  consufusionInterVal = setInterval(() => {
+    // Math.random
+    const randomNumber = Math.floor(Math.random() * 3); // 0,1,2
+    //console.log(randomNumber);
+    confuseThem.innerHTML = arr[randomNumber];
+  }, 200);
 }

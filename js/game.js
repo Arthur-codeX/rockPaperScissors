1013b87dc9febe9bb9eb7908d088bb65dbdcbe2a
Arthur-codeX <daniel.karanja239@gmail.com>
2025-06-20 17:23:52 +0300

    updated countdown

--- js/game.js
+++ js/game.js
@@ -3,6 +3,23 @@ const countdown = document.getElementById("countdown");
 const confuseThem = document.getElementById("confuse-them");
 const PlayBtn = document.getElementById("play-btn");
 
+let countDownValue = 15;
+
 // console.log(countdown);
 // console.log(confuseThem);
 // console.log(PlayBtn);
+PlayBtn.addEventListener("click", function () {});
+
+function StartCountDown() {
+  countDownValue = 15;
+  let interval = null;
+  interval = setInterval(function () {
+    if (countDownValue <= 0) {
+      clearInterval(interval); //Logic
+      countdown.innerText = 15;
+      return;
+    }
+    countdown.innerText = countDownValue - 1;
+    countDownValue = countDownValue - 1;
+  }, 1000);
+}

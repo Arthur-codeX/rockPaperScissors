50f815ed121c50e1428b83c077bb4d29b447682c
Arthur-codeX <daniel.karanja239@gmail.com>
2025-06-20 16:23:42 +0300

    added form submit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /// an event listner Callback
 
 const NameInput = document.getElementById("name-input");
+let name = "";
 
 // Access->update ,Access->add events,
 
@@ -12,14 +13,19 @@ const NameInput = document.getElementById("name-input");
 // input<>
 NameInput.addEventListener("input", nameInputHasChanged);
 
+function submitButtonClicked() {
+  let nameValue = document.getElementById("name-input").value;
+  name = nameValue;
+  window.location.href = "game.html";
+}
+
 function nameInputHasChanged() {
   //   console.log(NameInput);
   let nameValue = document.getElementById("name-input").value;
   let pSubmit = document.querySelector("#submit-button");
   if (nameValue.length >= 4) {
     // when the value name >4
-
-    pSubmit.innerHTML = `<button>Submit</button>`;
+    pSubmit.innerHTML = `<button onclick='submitButtonClicked()'>Submit</button>`;
     return;
   }
   pSubmit.innerHTML = ``;
